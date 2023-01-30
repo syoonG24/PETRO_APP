67da31d43b44583b0e6f709ff1cb37189f30f5fc
seokj <syoon@espol.edu.ec>
2023-01-29 20:02:27 -0500

    asd

--- APP.py
+++ APP.py
@@ -1,90 +1,88 @@
+# Import python libraries
 import streamlit as st
 import pandas as pd
-import plotly_express as px
+import matplotlib.pyplot as plt
+import welly
+import lasio
 from streamlit_option_menu import option_menu
 from PIL import Image
-from math import radians, acos, asin, cos, sin, atan, degrees, sqrt
-from bs4 import BeautifulSoup
-import warnings
+from io import StringIO
 
-# Insert an icon
-icon = Image.open("Resources/well.jpg")
 
-# State the design of the app
-st.set_page_config(page_title="Drilling App", page_icon=icon)
+# Insert icon of web app
+icon = Image.open("recursos/barril.jpg")
+# Page Layout
+st.set_page_config(page_title="Well Logs App")
 
-# Insert css codes to improve the design of the app
-st.markdown(
-    """
-<style>
-h1 {text-align: center;
-}
-body {background-color: #DCE3D5;
-      width: 1400px;
-      margin: 15px auto;
-}
-</style>""",
-    unsafe_allow_html=True,
-)
-
-# Title of the app
-st.title("Drilling Engineering App :link:")
-
-st.write("---")
-
-# Add information of the app
-st.markdown(
-    """ This app is used to visualize 3D Trajectories of directional wells, to upload csv files, 
-to call data, and to realize basic calculations.
-
-**Python Libraries:** Streamlit, pandas, plotly, PIL.
-"""
-)
-
-# Add additional information
-expander = st.expander("About")
-expander.write("This app is very useful for drilling projects")
-
-# Insert image
-image = Image.open("Resources/dd.jpg")
+# Title of app
+st.title("Well Logs Visualization")
+st.write("This logs contain information about petrophysical features of any well")
+image = Image.open("recursos/petro.jpg")
 st.image(image, width=100, use_column_width=True)
-
-# Insert video
-st.subheader("**Drilling Fundamentals**")
-video = open("Resources/drilling.mp4", "rb")
-st.video(video)
-
-# Add caption
-st.caption("Video about drilling fundamentals")
-
-# Sidebar
-Logo = Image.open("Resources/ESPOL.png")
-st.sidebar.image(Logo)
-
-# Add title to the sidebar section
-st.sidebar.title(":arrow_down: **Navigation**")
-
-# Upload files
-upload_file = st.sidebar.file_uploader("Upload your html file")
 
 # Pages
 with st.sidebar:
     options = option_menu(
-        menu_title="Menu",
-        options=["Home", "Dataframe", "Plots", "Petro Calculations"],
-        icons=["house", "tv-fill", "box", "calculator"],
+        menu_title="Main Menu",
+        options=["Home", "Data Information", "Logs Visualization","Petrophysics Calculation"],
+        icons=["house", "clipboard-data", "tv"],
     )
-# Useful functions
-def data(dataframe):
-    st.header("**Dataframe header**")
-    st.write(dataframe.head())
-    st.header("**Statistical information**")
-    st.write(dataframe.describe())
+
+# Options
+if options == "Data Information":
+    # number of file to upload
+    n_wells = int(st.number_input("Enter the well logs files"))
+    files = [
+        st.file_uploader(f"Upload the las file of well {n + 1}") for n in range(n_wells)
+    ]
+    if files is not None:
+        for log in files:
+            stri=[StringIO(log.getvalue().decode("utf-8"))]
+            for log in stri:
+                data=lasio.read(log).df()
+                well=[st.write(data)]
+                val=[st.write(data.columns)]
 
 
-if upload_file:
-    data_xml = BeautifulSoup(upload_file, 'html.parser')
-    params = set([str(tag.name) for tag in data_xml.find_all()])
-    df = pd.DataFrame()
-    for col in params:
-        df[col] = [float(x.text) for x in data_xml.find_all(col)]
+elif options == "Logs Visualization":
+    # number of file to upload
+    n_wells = int(st.number_input("Enter the well logs files"))
+    files = [
+        st.file_uploader(f"Upload the las file of well {n + 1}") for n in range(n_wells)
+    ]
+    if files is not None:
+        stri = [StringIO(log.getvalue().decode("utf-8")) for log in files]
+        for log in stri:
+            data = lasio.read(log).df()
+            fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(5, 10), sharey=True, gridspec_kw={"wspace": 0})
+            logs_to_plot = ["KLOGH", "PHIF", "SW"]
+
+            for ax, log in zip(axs, logs_to_plot):
+                ax.plot(data[log], data.index)
+                ax.set_title(log)
+                ax.xaxis.tick_top()
+
+            axs[0].set_ylim(None, None)
+            axs[0].invert_yaxis()
+
+            # plt.subplots_adjust(wspace=0)
+            graf=plt.show()
+            st.pyplot(graf)
+
+elif options== "Petrophysics Calculation":
+    n_wells = int(st.number_input("Enter the well logs files"))
+    files = [
+        st.file_uploader(f"Upload the las file of well {n + 1}") for n in range(n_wells)
+    ]
+    if files is not None:
+        stri = [StringIO(log.getvalue().decode("utf-8")) for log in files]
+        for log in stri:
+            data = lasio.read(log).df()
+
+            data['VSH'] = (data['GR'].max() - data['GR']) / (data['GR'].max()- data['GR'].min())
+            st.write(data['VSH'])
+
+
+
+
+
